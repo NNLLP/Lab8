63dc7a4ab87d1d0c4e282ee9797bb329b8912d68
NNLLP <98796613+NNLLP@users.noreply.github.com>
2024-12-07 00:00:34 +0700

    lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,50 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int age, height, bounty;
+string character;
+
+int main(){
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age > 25){
+        if(age > 60){
+            character = "Brook";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 500000000){
+                character = "Jinbe";
+            }
+            else{
+                character = "Franky";
+            }
+        }
+    }
+    else{
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height >= 100){
+            if(height >= 180){
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if(bounty > 1100000000){
+                    character = "Zoro";
+                }
+                else{
+                    character = "Sanji";
+                }
+            }
+            else{
+                character = "Usopp";
+            }
+        }
+        else{
+            character = "Chopper";
+        }
+    }
+    cout << "Your character = " << character << endl;
+}
